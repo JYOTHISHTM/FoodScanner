74866aa6eb534bc5d9f499b8ca0dc58869a86641
Jyothish T M <jothishtm2002@gmail.com>
2026-04-11 14:12:53 +0530

    fix(scanner): prevent multiple camera initialization using ref guard

--- src/modules/product/infrastructure/product.repository.ts
+++ src/modules/product/infrastructure/product.repository.ts
@@ -1,14 +1,3 @@
-// import axios from "axios";
-
-// export const fetchProductFromAPI = async (productId: string) => {
-//   const response = await axios.get(
-//     `https://world.openfoodfacts.org/api/v0/product/${productId}.json`
-//   );
-
-  
-
-//   return response.data.product;
-// };
 
 
 import axios from "axios";
