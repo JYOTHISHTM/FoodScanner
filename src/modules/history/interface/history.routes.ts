d22469c5b6fade800002af3b570b819e43540f5c
JYOTHISH T M <jothishtm2002@gmail.com>
2026-04-12 19:45:21 +0530

    feat: protect history routes with auth middleware

--- src/modules/history/interface/history.routes.ts
+++ src/modules/history/interface/history.routes.ts
@@ -1,9 +1,12 @@
 // routes/scanRoutes.ts
 import express from "express";
 import { getHistory } from "./history.controller";
+import { authMiddleware } from "../../../core/middlewares/auth.middleware";
+
+
 
 const router = express.Router();
 
-router.get("/", getHistory);
+router.get("/",authMiddleware, getHistory);
 
 export default router;
