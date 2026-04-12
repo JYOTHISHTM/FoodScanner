0c7ed58f23f505f6d1e8b6a1cc624877b3efe098
JYOTHISH T M <jothishtm2002@gmail.com>
2026-04-12 19:45:42 +0530

    feat: protect scan routes with auth middleware

--- src/modules/scan/interface/scan.routes.ts
+++ src/modules/scan/interface/scan.routes.ts
@@ -1,9 +1,11 @@
 // modules/scan/interface/scan.routes.ts
 import express from "express";
 import { scanProduct } from "./scan.controller";
+import { authMiddleware } from "../../../core/middlewares/auth.middleware";
+
 
 const router = express.Router();
 
-router.post("/", scanProduct);
+router.post("/",authMiddleware, scanProduct);
 
 export default router;  
