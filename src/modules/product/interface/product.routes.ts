c132170fe8fdc91f61ea30ca4ad92eaf29ac3d34
JYOTHISH T M <jothishtm2002@gmail.com>
2026-04-12 19:45:33 +0530

    feat: protect product routes with auth middleware

--- src/modules/product/interface/product.routes.ts
+++ src/modules/product/interface/product.routes.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { getProductByBarcodeController } from "./product.controller";
+import { authMiddleware } from "../../../core/middlewares/auth.middleware";
+
 
 const router = express.Router();
 
-router.get("/:productId", getProductByBarcodeController);
+router.get("/:productId",authMiddleware, getProductByBarcodeController);
 
 export default router;
