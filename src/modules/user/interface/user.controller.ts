4d8292b54a5fba713296783850c0be7838e7ad57
JYOTHISH T M <jothishtm2002@gmail.com>
2026-04-14 21:54:42 +0530

    refactor: unify response structure in user and history modules

--- src/modules/user/interface/user.controller.ts
+++ src/modules/user/interface/user.controller.ts
@@ -1,16 +1,26 @@
 import { Request, Response } from "express";
 import { UserService } from "../application/user.service";
 
+import { HTTP_STATUS } from "../../../core/constants/httpStatus";
+import { MESSAGES } from "../../../core/constants/messages";
+
 const service = new UserService();
 
 export const updateProfile = async (req: Request, res: Response) => {
-    try {
-        const userId = req.params.id as string;
+  try {
+    const userId = req.params.id as string;
 
-        const updated = await service.updateProfile(userId, req.body);
+    const updated = await service.updateProfile(userId, req.body);
 
-        res.json(updated);
-    } catch (err) {
-        res.status(500).json({ message: "Error updating profile" });
-    }
+    res.status(HTTP_STATUS.OK).json({
+      success: true,
+      message: MESSAGES.USER.PROFILE_UPDATED,
+      data: updated,
+    });
+  } catch (err: any) {
+    res.status(err.statusCode || HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message: err.message || MESSAGES.USER.PROFILE_UPDATE_FAILED,
+    });
+  }
 };
