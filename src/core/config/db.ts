5ed6adb4ebe5687e26ecc907902c8a9b20fe1e01
JYOTHISH T M <jothishtm2002@gmail.com>
2026-04-14 21:53:18 +0530

    refactor: use centralized messages in database config

--- src/core/config/db.ts
+++ src/core/config/db.ts
@@ -1,11 +1,17 @@
 import mongoose from "mongoose";
+import { MESSAGES } from "../constants/messages";
 
-export const connectDB=async ():Promise<void>=>{
-    try{
-        const connect =await mongoose.connect(process.env.MONGO_URI as string)
-        console.log(`DB connected :${connect.connection.host}`)
-    }catch(err){
-        console.error("DataBase Connection Failed",err)
-        
-    }
-} 
+export const connectDB = async (): Promise<void> => {
+  try {
+    const connect = await mongoose.connect(
+      process.env.MONGO_URI as string
+    );
+
+    console.log(
+      `${MESSAGES.COMMON.DB_CONNECTED}: ${connect.connection.host}`
+    );
+  } catch (err) {
+    console.error(`${MESSAGES.COMMON.DB_CONNECTION_FAILED}`, err);
+    process.exit(1); 
+  }
+};
